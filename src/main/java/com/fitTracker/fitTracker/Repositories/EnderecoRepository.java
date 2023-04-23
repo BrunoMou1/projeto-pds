fb15faa0df12df9031e7966c8733e6724e5d4a5d
humberto789 <humbertovitalino1@gmail.com>
2023-04-23 14:37:02 -0300

    Apagando os dados do endereço da pessoa

--- src/main/java/com/fitTracker/fitTracker/Repositories/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fitTracker.fitTracker.Repositories;
-
-import com.fitTracker.fitTracker.Models.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
